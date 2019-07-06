f49a59855f9cfadb9c037634c75e04a6abd1a8f2
osamamxd <osama@mxd3d.com>
2019-07-06 14:46:35 +0300

    Add Leaderboard Manager

---
+++ Petra Demo/Assets/Scripts/UI/LeaderboardManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardManager : MonoBehaviour {
+    public Image loader;
+    public Text loaderPercentage;
+    int currentValue;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        loader.fillAmount = currentValue;
+        loaderPercentage.text = currentValue.ToString() + "%";
+    }
+    public void SetLeaderboardValue(int deltaValue)
+    {
+        int oldValue = currentValue;
+        currentValue = (currentValue + deltaValue >= 100) ? 100 : currentValue + deltaValue;
+        loader.fillAmount = currentValue * 0.01f;
+        loaderPercentage.text = currentValue.ToString() + "%";
+    }
+    public void OpenLeaderboard()
+    {
+        anim.SetBool("open", true);
+        anim.SetBool("close", false);
+    }
+    public void CloseLeaderboard()
+    {
+        anim.SetBool("close", true);
+        anim.SetBool("open", false);
+    }
+}
