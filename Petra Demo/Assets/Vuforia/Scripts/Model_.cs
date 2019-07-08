526ec3af731df5cc00fa81f9ce46760cb8be47ab
remawy <remawy@mxd3d.com>
2019-07-08 16:09:37 +0300

    Create (start page,main page ,and activ AR Camera using Button

---
+++ Petra Demo/Assets/Vuforia/Scripts/Model_.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model_ : MonoBehaviour
+{
+   
+    public AudioClip audio;
+    bool isActiveSound = false;
+    public void playAudio()
+    {
+        transform.GetComponent<AudioSource>().clip = audio;
+        transform.GetComponent<AudioSource>().Play();
+    }
+  public void stopPlaying()
+    {
+        transform.GetComponent<AudioSource>().Stop();
+    }
+
+
+
+public void TrigerSound()
+{
+    isActiveSound = !isActiveSound;
+    ActiveSound();
+}
+
+void ActiveSound()
+{
+    if (isActiveSound)
+    {
+
+        print("isActiveSound" + isActiveSound);
+        transform.GetComponent<Model_>().playAudio();
+    }
+    else
+    {
+        transform.GetComponent<Model_>().stopPlaying();
+
+    }
+}
+    
+
+}
